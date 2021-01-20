c3f5d94ef2253eec00f8be89424a68f585a77875
danybb2020 <daniel.balderas@gmail.com>
2021-01-19 17:13:47 -0800

    eslint

--- build/index.js
+++ build/index.js
@@ -1,9 +1,8 @@
-import express = require("express");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
 const app = express();
 const port = 9000;
-
 app.get("/", (req, res) => res.send("Hello great world"));
-
 app.listen(port);
-
 console.log(`[app]: http://localhost:${port}`);
